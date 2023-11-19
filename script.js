42f54c761722867ea108bfde0ca313929a174dda
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2023-11-19 19:02:29 -0400

    final commits

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // JavaScript code here
 
-// Reference to the #generate element
+// Reference to the #generate element which is a button.
 var generateBtn = document.querySelector('#generate');
 
 // Function to write password to the #password input
@@ -10,6 +10,7 @@ function writePassword() {
   // Prompt the user for the password length until a valid input is provided
   var userPasswordLength;
   do {
+    // The "do while" loop ensures that the code for the prompt will be executed at least once and then repeated UNTIL the specified conditions within the "while" part are met and valid inputs from the user are given.
     userPasswordLength = parseInt(
       prompt(
         'Choose the length of your password. It MUST be between 8-128 characters'
@@ -21,35 +22,18 @@ function writePassword() {
     userPasswordLength > 128
   );
 
-  // User entered a valid password length
-  alert('Password length valid! Please continue');
-
-  // Ask the user if they want uppercase
-  var includeUppercase = prompt(
-    "Do you want to include any random UPPERCASE letters? Enter '1' for Yes and '0' for No"
-  );
-
-  if (includeUppercase === '1') {
-    alert('Sure! We will include Uppercase letters.');
-  } else if (includeUppercase === '0') {
-    alert("Okay, we won't include Uppercase letters.");
-  } else {
-    alert('Invalid answer. Try again.');
-    return;
-  }
-
   // Ask user if they want to include lowercase
-  var includeLowercase = prompt(
-    "Do you want to include any random lowercase letters? Enter '1' for Yes and '0' for No"
-  );
+  var includeLowercase;
+  do {
+    includeLowercase = prompt(
+      "Do you want to include any random lowercase letters? Enter '1' for Yes and '0' for No"
+    );
+  } while (includeLowercase !== '1' && includeLowercase !== '0');
 
   if (includeLowercase === '1') {
     alert('Sure! We will include lowercase letters.');
-  } else if (includeLowercase === '0') {
-    alert("Okay, we won't include lowercase letters.");
   } else {
-    alert('Invalid answer. Try again.');
-    return;
+    alert("Okay, we won't include lowercase letters.");
   }
 
   // Ask user if they want to include numbers 0-9
@@ -90,6 +74,9 @@ function writePassword() {
   var specialChars = '!@#$%^&*+';
 
   // Include uppercase characters
+  var includeUppercase = prompt(
+    "Do you want to include any random UPPERCASE letters? Enter '1' for Yes and '0' for No"
+  );
   if (includeUppercase === '1') {
     characters += uppercaseChars;
   }
@@ -123,6 +110,8 @@ function writePassword() {
   }
 
   alert('Generated Password: ' + generatedPassword);
+
+  return; //this should stop the program after the pass word is gneerated and displayed.
 }
 
 // Add event listener to the #generate button
