ffbf42e09fa2b39ca66711f4f1d12374eec8d0fa
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2023-11-18 22:03:55 -0400

    took a different approach to this after thinking about it. its not the cleanest code but its human readable.

--- assets/script.js
+++ assets/script.js
@@ -6,47 +6,95 @@ var generateBtn = document.querySelector('#generate');
 // Write password to the #password input
 function writePassword() {
   console.log('Running from my button Click');
-  function GenRandoPass(length, numbers, lowercase, uppercase, spechar) {
-    var allowedInputs = '';
 
-    if (numbers) {
-      characters += '1234567890';
-    }
+  // 1. Prompt the user for the password length until a valid input is provided
+  var userPasswordLength;
+  do {
+    userPasswordLength = parseInt(
+      prompt(
+        'Choose the length of your password. It MUST be between 8-128 characters'
+      )
+    );
+  } while (
+    isNaN(userPasswordLength) ||
+    userPasswordLength < 8 ||
+    userPasswordLength > 128
+  );
 
-    if (lowercase) {
-      characters += 'abcdefghijklmnopqrstuvwxyz';
-    }
+  // userPasswordLength is a valid number between 8 and 128
+  alert('Password length valid! Please continue');
 
-    if (uppercase) {
-      characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    }
+  // 2. Ask the user if they want uppercase
+  var includeUppercase = prompt(
+    "Do you want to include any random UPPERCASE letters? Enter '1' for Yes and '0' for No"
+  );
 
-    if (spechar) {
-      characters += '!@#$%^&*()-_=+';
-    }
+  if (includeUppercase === '1') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert('Sure! We will include Uppercase letters.');
+  } else if (includeUppercase === '0') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert("Okay, we won't include Uppercase letters.");
+  } else {
+    alert('Invalid answer. Try again.');
   }
-  //1.Ask user for length of password between 8-128 char
-  //1.1 Validate
-  //1.1.1If length is not between 8-128 char ...? exit and alert and restart
-  //1.1.2If it is between 8-128char, move on to next criteria
-  //2. Ask which char types to include:
-  // 2.2lower? confirm y/n
-  // validate
-  // 2.3UPPER? confirm y/n
-  // validate
-  // 2.4numeric? confirm y/n
-  // validate
-  // 2.5$pecial? - confirm y/n
-  // validate
-  //at least 1 char type must* be selected
-  //3.Store responses
-  //3.3 Randomly combine user specifications into their password (generate pass when all steps have been answered)
-  //display the pass
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  // 3. Ask user if they want to include lowercase.
+  var includeLowercase = prompt(
+    "Do you want to include any random lowercase letters? Enter '1' for Yes and '0' for No"
+  );
+  if (includeLowercase === '1') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert('Sure! We will include lowercase letters.');
+  } else if (includeLowercase === '0') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert("Okay, we won't include lowercase letters.");
+  } else {
+    alert('Invalid answer. Try again.');
+  }
+
+  // 4. Ask user if they want to include numbers 0-9
+  var includeNum = prompt(
+    "Do you want to include any random numbers from 0-9? Enter '1' for Yes and '0' for No"
+  );
+
+  if (includeNum === '1') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert('Sure! We will include numbers.');
+  } else if (includeNum === '0') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert("Okay, we won't include any numbers.");
+  } else {
+    alert('Invalid answer. Try again.');
+  }
+
+  // 5. Ask user if they want to include special chars !@#$%^&*+
+  var includeNSpeChar = prompt(
+    "Do you want to include any random Special Characters? They will be from a random selection of !@#$%^&*+. Enter '1' for Yes and '0' for No"
+  );
+
+  if (includeNSpeChar === '1') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert('Sure! We will include special characters.');
+  } else if (includeNSpeChar === '0') {
+    // Corrected: compare against string
+    // Acknowledge their answer
+    alert("Okay, we won't include any special characters.");
+  } else {
+    alert('Invalid answer. Try again.');
+  }
+
+  // Continue with other questions or password generation logic as needed
+  // For now, displaying a message
+  alert('Password generated and displayed!');
 }
 
-// Add event listener to generate button
+// Add event listener to the #generate button
 generateBtn.addEventListener('click', writePassword);
